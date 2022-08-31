95251a399601e8c41de941b07560ae1ef1ea5234
yel-aziz <elaziziyassir2@gmail.com>
2022-08-31 23:46:53 +0100

    The second way to access the properties of an object is bracket notation ([])

---
+++ functions/readObjectBrackets/readObjectsWithBrackets.js
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readObjectsWithBrackets.js                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 23:46:15 by yel-aziz          #+#    #+#             */
+/*   Updated: 2022/08/31 23:46:22 by yel-aziz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  const entreeValue = testObj["an entree"];  
+  const drinkValue = testObj["the drink"];   
