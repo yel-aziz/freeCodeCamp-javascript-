570343a74610ef92fb7ef889d3c720e393c1ac8a
yel-aziz <elaziziyassir2@gmail.com>
2022-08-31 23:43:59 +0100

    read an object with dot

---
+++ functions/readObject/readObject.js
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readObject.js                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 23:43:38 by yel-aziz          #+#    #+#             */
+/*   Updated: 2022/08/31 23:43:44 by yel-aziz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  
+  const hatValue = testObj.hat;      
+  const shirtValue = testObj.shirt;
